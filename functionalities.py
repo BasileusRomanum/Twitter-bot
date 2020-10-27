ee98478d31a3497dc27d87d5242c0d9ee0305318
Jędrzej <43277806+BasileusRomanum@users.noreply.github.com>
2020-10-27 12:09:40 +0100

    Update functionalities.py

--- functionalities.py
+++ functionalities.py
@@ -1,8 +1,12 @@
 import tweepy
 import logging
-import time
+import random
+import json
+from time import sleep
+from quotes import cytaty
 
 logger = logging.getLogger()
+
 
 def followFollowers(api):
     logger.info("Odczytywanie i followowanie followersów")
@@ -37,33 +41,73 @@ class Favoryzuj(tweepy.StreamListener):
     def on_error(self, status):
         logger.error(status)
 
+#Generator of "quotes" by infamous polish politician.
+def korwinizmy():
+    hasztagi = '\n#Korwin #MyslDnia'
+    while True:
+        try:
+            for i in range(0, len(cytaty)):
+                result.append(cytaty[i][random.randint(0, len(cytaty[i])-1)])
+        except:
+            logger.info("Error during creating Korwin's quote")
+            result.clear()
+            continue
+        logger.info("Korwin quote generated")
+        if len("Myśl według Korwina na teraz: " + '"' + " ".join(result) + hasztagi) <=280:
+            api.update_status("Myśl według Korwina na teraz: " + '"' + " ".join(result) + hasztagi)
+            logger.info("Korwinizm został umieszczony na twitterze")
+            result.clear()
+            sleep(60)
+        else:
+            logger.error("Character limit exceeded")
+            result.clear()
+            continue
 
-cytaty = [
-    ['Proszę zwrócić uwagę, że',
-    'I tak mam trzy razy mniej czasu, więc proszę mi pozwolić powiedzieć, że',
-    'Państwo się śmieją, ale', 'Ja nie potrzebowałem edukacji seksualnej, żeby wiedzieć, że',
-    'No niestety',
-    'Gdzie leży przyczyna problemu? Ja państwu powiem:',
-    'Państwo chyba nie widzą, że',
-    'Oświadczam kategorycznie:',
-    'Powtarzam',
-    'Powiedzmy z całą mocą:',
-    'W Polsce dzisiaj',
-    'Państwo sobie nie zdają sprawy, że',
-    'To ja przepraszam bardzo:',
-    'Otóż nie wiem, czy pan wie, że',
-    'Yyyyyy...',
-    'Ja chcę powiedzieć jedną rzecz:',
-    'Trzeba powiedzieć jasno',
-    'Jak powiedział wybitny krakowianin,',
-    'Proszę mnie dobrze zrozumieć',
-    'Ja chciałem państwu przypomnieć, że',
-    'Niech państwo nie mają złudzeń:',
-    'Powiedzmy to wyraźnie:'],
-    ['właściciele niewolników',
-    'związkowcy',
-    'trockiści', 'tak zwane dzieci kwiaty', 'rozmaici urzędnicy', 'federaści', 'etatyści', 'ci durnie i złodzieje', 'ludzie wybrani głosami meneli spod budki z piwem', 'socjaliści pobożni', 'socjaliści bezbożni', 'komuniści z krzyżem w zębach', 'agenci obcych służb', 'członkowie Bandy Czworga', 'pseudo-masoni z Wielkiego Wschodu Francji', 'przedstawiciele czerwonej hołoty', 'ci wszyscy (tfu) geje', 'funkcjonariusze reżymowej telewizj', 'tak zwani ekolodzy', 'ci wszyscy (tfu) demokracji', 'agenci bezpieki', 'feminazistki'],
-    ['po przeczytaniu Manifestu Komunistycznego', 'którymi się brzydzę', 'których nienawidzę', 'z okolic Gazety Wyborczej', 'czyli taka żydokomuna', 'odkąd zniesiono karę śmierci', 'którymi pogardzam', 'któych miejsce w normalnym kraju jest w więzieniu', 'na polecenie Brukseli', 'posłusznie', ' bezmyślnie', 'z nieprawdopodobną pogardą dla człowieka', 'za pieniądze podatników', 'zgodnie z ideologią LGBTQZ', 'za wszelką cenę', 'zupełnie bezkarnie', 'całkowicie bezczelnie', 'o poglądach na lewo od komunizmu', 'celowo i świadomie', 'z premedytacją', 'od czasów Okrągłego Stołu', 'w ramach postępu'],
-    ['udają homoseksualistów', 'niszczą rodzinę', 'idą do polityki', 'zakazują góralom robienia oscypków', 'organizują paraolimpiady', 'wprowadzają ustrój w którym raz na cztery lata można wybrać sobie pana', 'ustawiają fotoradary', 'wprowadzają dotacje', 'wydzielają buspasy', 'podnoszą wiek emerytalny', 'rżną głupa', 'odbierają dzieci rodzicom', 'wprowadzają absurdalne przepisy', 'umieszczają dzieci w szkołach koedukacyjnych', 'wprowadzają parytety', 'nawołują do podniesienia podatku', 'próbują wyrzucić kierowców z miast', 'próbują skłócić Polskę z Rosją', 'głoszą brednie o globalnym ociepleniu', 'zakazują posiadania broni', 'nie dopuszczają prawicy do władzy', 'uczą dzieci homoseksualizmu'],
-    ['żeby poddawać wszystkich tresurze', 'bo taka jest ich natura', 'bo chcą wszystko kontrolować', 'bo nie rozumieją, że socjalizm nie działa', 'żeby wreszcie zapanował socjalizm', 'dokładnie tak jak tow. Janosik', 'zamiast pozwolić ludziom zarabiać', 'żeby wyrwać kobiety z domu', 'bo tak jest w interesie tak zwanych ludzi pracy', 'zamiast pozwolić decydować konsumentowi', 'żeby nie opłacało się mieć dzieci', 'zamiast obniżyć podatki', 'bo nie rozumieją, że selekcja naturalna jest czymś dobrym', 'żeby mężczyźni przestali być agresywni', 'bo dzięki temu mogą brać łapówki', 'bo dzięki temu mogą kraść', 'bo dostają za to pieniądze', 'bo tak się uczy w państwowej szkole', 'bo bez tego (tfu) demokracja nie może istnieć', 'bo głupich jest więcej niż mądrych', 'bo chcą tworzyć raj na Ziemi', 'bo chcą niszczyć cywilizację białego człowieka'],
-    ['co ma zresztą tyle samo sensu, co zawody w szachach dla debili.', 'co zostało dokładnie zaplanowane w Magdalence przez śp. generała Kiszczaka.', 'i trzeba być idiotą, żeby ten system popierać.', 'ale nawet ja jeszcze dożyję normalnych czasów.', 'co dowodzi, że wyskrobano nie tych, co trzeba.', 'a zwykłym ludziom wmawiają, że im coś "dadzą".', '- cóż: chcieliście (tfu) demokracji, to macie.', 'dlatego trzeba zlikwidować koryto, a nie zmieniać świnie.', 'a wystarczyłoby przestać wypłacać zasiłki.', 'podczas gdy normalni ludzi uważani są za dziwaków.', 'co w wieku XIX po prostu by wyśmiano.', '- dlatego w społeczeństwie jest równość, a powinno być rozwarstwienie.', 'co prowadzi Polskę do katastrofy', '- dlatego trzeba przywrócić normalność.', 'ale w wolnej Polsce pójdą siedzieć.', 'przez kolejne kadencje.', 'o czym się nie mówi.', 'i właśnie dlatego Europa umiera.', 'ale przyjdą muzułmanie i zrobią porządek.', '- tak samo zresztą jak za Hitlera.', '- proszę zobaczyć, co się dzieje na Zachodzie, jeśli mi państwo nie wierzą.', 'co lat temu sto nikomu nie przyszłoby nawet do głowy']]
+#Monitoring of dms through while loop
+def dmy(api, flaga=False):
+    odpowiedziane = []
+    answerText = "Example string"
+    while True:
+        dmy = api.list_direct_messages()
+        print(type(dmy))
+        for message in dmy:
+            if message.created_timestamp not in odpowiedziane:
+                try:
+                    odpowiedziane.append(message.created_timestamp)
+                    print(message.message_create['sender_id'])
+                    api.send_direct_message(message.message_create['sender_id'], answerText)
+                except:
+                    logger.error("Error during answering to a message", exc_info=True)
+                    sleep(random.randint(1,5))
+                    pass
+        if flaga:
+            break
+        sleep(5)
+
+#As to not use while loop monitoring dms has been rewritten to use Cursor object.
+#And it works. Although, I seem to be unable to access dm's text this way. Why should I tho, I don't know.
+#I've just been trying to do it 'till I've askedy myself "Why?"
+def dmyKursorem(api):
+#Local variables
+    has_been_answered = False
+    dm_answer = "Hello beatiful person!"
+    logger.info("Trying to load messages id")
+#Loading up json with already answered messages. And if it's empty, then create an empty dictionary
+    try:
+        messagesDictionary = json.load(open('msgID.json'))
+        logger.info("IDs loaded sucesfully")
+    except ValueError:
+        messagesDictionary = {}
+
+        logger.error("Unable to load from msgID.json. Created empty dictionary", exc_info=True)
+    #Json that temp. stores id and timestamp of each dm that has been responded too.
+    #It's gonna be later dumped into a file.
+    for dm in tweepy.Cursor(api.list_direct_messages).items():
+        for storedMessage in messagesDictionary.items():
+            if dm.id == storedMessage['id']:
+                has_been_answered = True
+                break
+        if not has_been_answered:
+            api.send_direct_message(dm.message_create['sender_id'], dm_answer)
+            messagesDictionary["".join(['message', time.time()])] = {'id':dm.id, 'timestamp':dm.timestamp}
+            open("msgID.json", 'w').write(messagesDictionary)
